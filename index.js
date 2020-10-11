02b3c6c4939bbd43a3a6713f6e84fa6cc926444e
kggu <miikkakortelainen1@gmail.com>
2020-10-11 10:40:02 +0300

    Add local image uploading

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const multer = require("multer");
+const path = require("path");
+
 const port = 3000;
 const app = express();
 app.use(bodyParser.json());
@@ -21,6 +24,41 @@ const { authBasic } = require("./services/auth");
   - more routes
     /logout
 */
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+
+  // By default, multer removes file extensions
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image! Please upload an image.", 400), false);
+  }
+};
+
+app.post("/multi", (req, res) => {
+  let upload = multer({
+    storage: storage,
+    fileFilter: multerFilter,
+  }).array("image", 4);
+
+  upload(req, res, function (err) {
+    const files = req.files;
+    let index, len;
+    console.log(files.length);
+    let result = "Uploaded " + files.length + " images.";
+
+    res.send(result);
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("kauppa-api");
@@ -44,8 +82,6 @@ app.delete("/postings/:id", authBasic, PostService.deletePosting);
 app.get("/postings/search/", PostService.searchPostings);
 
 app.get("/postings/:id", PostService.getPosting);
-
-
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
