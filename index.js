ea7f2dce97cd479340b88eeae862e269a3b46c54
kggu <miikkakortelainen1@gmail.com>
2020-10-13 10:57:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const env = require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
 
